b380a801cb2684ab40a194ae3b38db908d7403a7
liacdev <jagoblacksky@gmail.com>
2022-10-27 18:30:58 -0500

    added beginning code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use std::io;
+
 fn main() {
     // This is a guessing game built in Rust language
-    println!("Guessing game!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+    
+    let mut guess = String::new();
+    
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+    println!("You guessed: {guess}");
 }
