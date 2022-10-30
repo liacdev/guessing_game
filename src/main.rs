1d9c3501d2ad55ae0731b452cf6082f873af7376
liacdev <jagoblacksky@gmail.com>
2022-10-30 13:05:28 -0500

    handling invalid input

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ fn main() {
     println!("The secret number is: {secret_number}");
 
     loop {
-        println!("Please input your guess.");
         
         println!("Please input your guess.");
         
@@ -22,7 +21,10 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
         
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         
         println!("You guessed: {guess}");
         
