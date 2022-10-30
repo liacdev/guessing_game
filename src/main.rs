8295624a28436d3355fb820188353e9b97f1b88a
liacdev <jagoblacksky@gmail.com>
2022-10-29 22:07:27 -0500

    random lib and code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
+use rand:: Rng;
 
 fn main() {
     // This is a guessing game built in Rust language
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
     
@@ -13,4 +18,11 @@ fn main() {
         .expect("Failed to read line");
     
     println!("You guessed: {guess}");
+// Test code 
+
+    let x = 5;
+    let y = 10;
+
+    println!("x = {} and y = {}", x, y);
+
 }
